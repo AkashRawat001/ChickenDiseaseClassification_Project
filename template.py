31f9977e2efa78d2b805ea876b7fb200e5eefc39
Akash Rawat <rawatakash789@gmail.com>
2024-04-11 23:20:07 +0530

    Data Ingestion Completed

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level = logging.INFO, format ='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
 project_name = "cnnClassifier"
@@ -13,7 +13,7 @@ list_of_files = [
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration.py"
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
@@ -24,18 +24,25 @@ list_of_files = [
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
+
+
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir ,filename = os.path.split(filepath)
+    filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
+
+    if filedir !="":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
-        logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
     else:
-        logging.info(f"File already exists: {filepath}")
+        logging.info(f"{filename} is already exists")
